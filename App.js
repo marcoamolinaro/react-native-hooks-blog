47e72c6202614d95ad39c2455d82b363243c693f
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2020-01-20 10:03:01 -0300

    138. Adding a Creation Screen

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { Provider } from './src/context/BlogContext';
 import IndexScreen from './src/screens/IndexScreen';
 import ShowScreen from './src/screens/ShowScreen';
-import { Provider } from './src/context/BlogContext';
+import CreateSreen from './src/screens/CreateScreen';
+
 
 const navigator = createStackNavigator({
   Index: IndexScreen,
-  Show: ShowScreen
+  Show: ShowScreen,
+  Create: CreateSreen
 }, {
   initialRouteName: 'Index',
   defaultNavigationOptions: {
---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CreateScreen = () => {
+
+    return (
+        <View>
+            <Text>CreateScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
