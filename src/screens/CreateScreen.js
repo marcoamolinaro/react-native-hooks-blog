312b85501766ef13c6901df0580e84e4d70cbdc4
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2020-01-20 16:41:14 -0300

    141. Saving a New Post

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,10 +1,12 @@
 import React, { useContext, useState } from 'react';
 import { View, Text, TextInput, StyleSheet, Button} from 'react-native';
+import { Context } from '../context/BlogContext';
 
 const CreateScreen = () => {
 
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+    const { addBlogPost } = useContext(Context);
 
     return (
         <View>
@@ -12,7 +14,9 @@ const CreateScreen = () => {
             <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)}/>
             <Text style={styles.label}>Enter Content</Text>
             <TextInput style={styles.input} value={content} onChangeText={text => setContent(text)}/>
-            <Button title='Add Blog Post'/>
+            <Button 
+                onPress={() => addBlogPost(title, content)}
+                title='Add Blog Post'/>
         </View>
     );
 };
