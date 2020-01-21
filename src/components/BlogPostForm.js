93b5f8e6b5f4087c51590c1a0bc1ba6272f65b2f
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2020-01-21 20:12:10 -0300

    147. Customizing OnSubmit

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
-const BlogPostForm = () => {
+const BlogPostForm = ({onSubmit}) => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
 
@@ -12,7 +12,9 @@ const BlogPostForm = () => {
             <Text style={styles.label}>Enter Content</Text>
             <TextInput style={styles.input} value={content} onChangeText={text => setContent(text)}/>
             <Button 
-                title='Save Blog Post'/>
+                title='Save Blog Post'
+                onPress={() => onSubmit(title, content)}
+            />
         </View>
     );
 };
